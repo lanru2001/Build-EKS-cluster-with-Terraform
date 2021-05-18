65e1d146f0315e36458f18e65d1198bf658c1249
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-05-17 19:13:04 -0500

    Update and rename main.tf to backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
-  backend "s3" 
+  backend "s3" {
     bucket               = "my-vibrant-and-nifty-app-infra"
     key                  = "tf-state.json"
     region               = "us-west-2"
     workspace_key_prefix = "environment"}
+  }
 }
-
 
