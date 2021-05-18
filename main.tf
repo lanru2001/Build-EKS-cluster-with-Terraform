8dd60993e95572b303c4aa2514a5d880891dff31
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-05-17 19:17:45 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,5 +50,71 @@ module "vpc" {
   }
 }
 
+# create EKS cluster
+module "eks-cluster" {
+  source           = "terraform-aws-modules/eks/aws"
+  version          = "12.1.0"
+  cluster_name     = "${var.cluster_name}"
+  cluster_version  = "1.16"
+  write_kubeconfig = false
 
+  subnets = module.vpc.private_subnets
+  vpc_id  = module.vpc.vpc_id
 
+  worker_groups_launch_template = local.worker_groups_launch_template
+
+  # map developer & admin ARNs as kubernetes Users
+  map_users = concat(local.admin_user_map_users, local.developer_user_map_users)
+}
+
+# get EKS cluster info to configure Kubernetes and Helm providers
+data "aws_eks_cluster" "cluster" {
+  name = module.eks-cluster.cluster_id
+}
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks-cluster.cluster_id
+}
+
+# get EKS authentication for being able to manage k8s objects from terraform
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.9"
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    load_config_file       = false
+  }
+  version = "~> 1.2"
+}
+
+# deploy spot termination handler
+resource "helm_release" "spot_termination_handler" {
+  name       = var.spot_termination_handler_chart_name
+  chart      = var.spot_termination_handler_chart_name
+  repository = var.spot_termination_handler_chart_repo
+  version    = var.spot_termination_handler_chart_version
+  namespace  = var.spot_termination_handler_chart_namespace
+}
+
+# add spot fleet Autoscaling policy
+resource "aws_autoscaling_policy" "eks_autoscaling_policy" {
+  count = length(local.worker_groups_launch_template)
+
+  name                   = "${module.eks-cluster.workers_asg_names[count.index]}-autoscaling-policy"
+  autoscaling_group_name = module.eks-cluster.workers_asg_names[count.index]
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = var.autoscaling_average_cpu
+  }
+}
