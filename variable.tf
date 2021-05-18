ee9ae218ab62cf628afe46fa7870c36506ecaa83
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-05-17 19:14:59 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -91,4 +91,77 @@ module "vpc" {
   }
 }
 
+# create some variables
+variable "admin_users" {
+  type        = list(string)
+  description = "List of Kubernetes admins."
+}
+variable "developer_users" {
+  type        = list(string)
+  description = "List of Kubernetes developers."
+}
+variable "asg_instance_types" {
+  type        = list(string)
+  description = "List of EC2 instance machine types to be used in EKS."
+}
+variable "autoscaling_minimum_size_by_az" {
+  type        = number
+  description = "Minimum number of EC2 instances to autoscale our EKS cluster on each AZ."
+}
+variable "autoscaling_maximum_size_by_az" {
+  type        = number
+  description = "Maximum number of EC2 instances to autoscale our EKS cluster on each AZ."
+}
+variable "autoscaling_average_cpu" {
+  type        = number
+  description = "Average CPU threshold to autoscale EKS EC2 instances."
+}
+variable "spot_termination_handler_chart_name" {
+  type        = string
+  description = "EKS Spot termination handler Helm chart name."
+}
+variable "spot_termination_handler_chart_repo" {
+  type        = string
+  description = "EKS Spot termination handler Helm repository name."
+}
+variable "spot_termination_handler_chart_version" {
+  type        = string
+  description = "EKS Spot termination handler Helm chart version."
+}
+variable "spot_termination_handler_chart_namespace" {
+  type        = string
+  description = "Kubernetes namespace to deploy EKS Spot termination handler Helm chart."
+}
 
+# render Admin & Developer users list with the structure required by EKS module
+locals {
+  admin_user_map_users = [
+    for admin_user in var.admin_users :
+    {
+      userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${admin_user}"
+      username = admin_user
+      groups   = ["system:masters"]
+    }
+  ]
+  developer_user_map_users = [
+    for developer_user in var.developer_users :
+    {
+      userarn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${developer_user}"
+      username = developer_user
+      groups   = ["${var.name_prefix}-developers"]
+    }
+  ]
+  worker_groups_launch_template = [
+    {
+      override_instance_types = var.asg_instance_types
+      asg_desired_capacity    = var.autoscaling_minimum_size_by_az * length(data.aws_availability_zones.available_azs.zone_ids)
+      asg_min_size            = var.autoscaling_minimum_size_by_az * length(data.aws_availability_zones.available_azs.zone_ids)
+      asg_max_size            = var.autoscaling_maximum_size_by_az * length(data.aws_availability_zones.available_azs.zone_ids)
+      kubelet_extra_args      = "--node-labels=node.kubernetes.io/lifecycle=spot" # use Spot EC2 instances to save some money and scale more
+      public_ip               = true
+    },
+  ]
+}
+    
+    
+    
