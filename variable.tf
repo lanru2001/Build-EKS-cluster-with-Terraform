668161a80dc8fd0217a2ecf97b95b3fbdb2c7c25
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-05-17 19:23:23 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -185,5 +185,9 @@ locals {
   ]
 }
     
-    
+# create some variables
+variable "deployments_subdomains" {
+  type        = list(string)
+  description = "List of subdomains to be routed to Kubernetes Services."
+}
     
