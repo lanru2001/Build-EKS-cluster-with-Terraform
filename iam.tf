9b8d3b0e5c177fa01ded6dfd7466876f0b207052
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-06-08 14:09:23 -0500

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,6 +1,6 @@
-resource "aws_iam_role" "scholar" {
-  name = "eks-cluster-scholar"
-
+#EKS Cluster IAM Role
+resource "aws_iam_role" "eks_cluster" {
+  name = "${var.eks_cluster_name}-cluster-${var.environment}"
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -17,14 +17,76 @@ resource "aws_iam_role" "scholar" {
 POLICY
 }
 
-#EKSClusterPolicy
-resource "aws_iam_role_policy_attachment" "scholar_AmazonEKSClusterPolicy" {
+#EKS Cluster and service policies 
+resource "aws_iam_role_policy_attachment" "aws_eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.scholar.name
+  role       = "${aws_iam_role.eks_cluster.name}"
+}
+resource "aws_iam_role_policy_attachment" "aws_eks_service_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = "${aws_iam_role.eks_cluster.name}"
 }
 
-# EKSServicePolicy
-resource "aws_iam_role_policy_attachment" "scholar_AmazonEKSServicePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = aws_iam_role.scholar.name
+#Worker Node Group IAM Role
+resource "aws_iam_role" "eks_nodes" {
+   name                 = "${var.eks_cluster_name}-worker-${var.environment}"
+   assume_role_policy   = data.aws_iam_policy_document.assume_workers.json
+}
+
+data "aws_iam_policy_document" "assume_workers" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+       type        = "Service"
+       identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+#EKS worker node policies 
+resource "aws_iam_role_policy_attachment" "aws_eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "aws_eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_read_only" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+#Autoscaling policies 
+resource "aws_iam_policy" "cluster_autoscaler_policy" {
+  name        = "ClusterAutoScaler"
+  description = "Give the worker node running the Cluster Autoscaler access to required resources and actions"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "autoscaling:DescribeAutoScalingGroups",
+                "autoscaling:DescribeAutoScalingInstances",
+                "autoscaling:DescribeLaunchConfigurations",
+                "autoscaling:DescribeTags",
+                "autoscaling:SetDesiredCapacity",
+                "autoscaling:TerminateInstanceInAutoScalingGroup"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+#Autoscaling policy attachment 
+resource "aws_iam_role_policy_attachment" "cluster_autoscaler" {
+  policy_arn = aws_iam_policy.cluster_autoscaler_policy.arn
+  role = aws_iam_role.eks_nodes.name
 }
